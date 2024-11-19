f2a279e88b464d4c08073e71f180ef12e3728ade
HyunSu1768 <azxcv1768@gmail.com>
2024-11-19 15:30:04 +0900

    feat(ipnet.go): IPv4 주소 파싱 기능 추가 - IP4 주소를 바이트 배열에서 변환하는 기능을 추가 - 문자열에서 IP4 주소를 파싱하고 오류 처리를 추가 - 잘못된 IPv4 주소에 대한 패닉 처리 추가

--- pkg/ip/ipnet.go
+++ pkg/ip/ipnet.go
@@ -1,5 +1,10 @@
 package ip
 
+import (
+	"errors"
+	"net"
+)
+
 type IP4 uint32
 
 func FromBytes(ip []byte) IP4 {
@@ -7,4 +12,31 @@ func FromBytes(ip []byte) IP4 {
 		(uint32(ip[2]) << 8) |
 		(uint32(ip[1]) << 16) |
 		(uint32(ip[0]) << 24))
+}
+
+func FromIP(ip net.IP) IP4 {
+	ipv4 := ip.To4()
+
+	if ipv4 == nil {
+		panic("주소가 ipv4가 아닙니다")
+	}
+
+	return FromBytes(ip)
+}
+
+func ParseIP4(s string) (IP4, error) {
+	ip := net.ParseIP(s)
+
+	if ip == nil {
+		return IP4(0), errors.New("올바르지 않은 IP 포멧입니다")
+	}
+	return FromIP(ip), nil
+}
+
+func MustParseIP4(s string) IP4 {
+	ipv4, err := ParseIP4(s)
+	if err != nil {
+		panic(err)
+	}
+	return ipv4
 }
