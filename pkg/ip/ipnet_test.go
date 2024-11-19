0ecb886df669f1a94e1f6f17246e47ae3a38c81e
HyunSu1768 <azxcv1768@gmail.com>
2024-11-19 18:57:10 +0900

    test(ipnet_test.go): IP4Net 관련 테스트 케이스 추가 IP4Net의 다양한 기능을 검증하기 위해 테스트 케이스를 추가하여 정확성을 높이고 코드의 신뢰성을 향상시킴.

--- pkg/ip/ipnet_test.go
+++ pkg/ip/ipnet_test.go
@@ -85,3 +85,57 @@ func TestIP4(t *testing.T) {
 		}
 	}
 }
+
+func TestIP4Net(t *testing.T) {
+	n1 := makeIP4Net("1.2.3.0", 24)
+
+	if n1.ToIPNet().String() != "1.2.3.0/24" {
+		t.Error("ToIPNet 실패")
+	}
+
+	if n1.StringSep("*", "/") != "1*2*3*0/24" {
+		t.Error("StringSep 실패")
+	}
+
+	if n1.Mask() != 0xFFFFFF00 {
+		t.Error("Mask 실패")
+	}
+
+	if !n1.Overlaps(n1) {
+		t.Errorf("%s은 %s를 Overlaps 하지 않는다.", n1, n1)
+	}
+
+	n2 := makeIP4Net("1.2.0.0", 16)
+	if !n1.Overlaps(n2) {
+		t.Errorf("%s은 %s를 Overlaps 하지 않는다.", n1, n2)
+	}
+
+	n2 = makeIP4Net("7.2.4.0", 22)
+	if n1.Overlaps(n2) {
+		t.Errorf("%s은 %s을 Overlaps 한다.", n1, n2)
+	}
+
+	if !n1.Contains(mkIP4("1.2.3.4")) {
+		t.Error("Contains 실패")
+	}
+
+	if !n1.Contains(mkIP4("1.2.3.0")) {
+		t.Error("Contains 실패")
+	}
+
+	if n1.Contains(mkIP4("2.3.4.5")) {
+		t.Error("Contains 실패")
+	}
+
+	j, err := json.Marshal(n1)
+	if err != nil {
+		t.Error("IP4NET Marshal 실패 : ", err)
+	} else if string(j) != `"1.2.3.0/24"` {
+		t.Error("예기치 못한 이유로 IP4NET Marshal 실패 : ", err)
+	}
+
+	n1.IncrementIP()
+	if n1.String() != "1.2.3.1/24" {
+		t.Error("IncrementIP 실패")
+	}
+}
