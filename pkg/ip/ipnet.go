e90e093a8b2ee683788969ca3bd0e210673e9645
HyunSu1768 <azxcv1768@gmail.com>
2024-11-19 15:54:36 +0900

    feat(ip): 네이티브 엔디안 감지 및 IP4 구조체에 메서드 추가 - NativeEndian 변수를 통해 시스템의 엔디안 형식을 감지 - IP4 구조체에 옥텟, IP 변환, 네트워크 순서 변환, 문자열 변환 메서드 추가

--- pkg/ip/ipnet.go
+++ pkg/ip/ipnet.go
@@ -2,6 +2,7 @@ package ip
 
 import (
 	"errors"
+	"fmt"
 	"net"
 )
 
@@ -39,4 +40,31 @@ func MustParseIP4(s string) IP4 {
 		panic(err)
 	}
 	return ipv4
+}
+
+func (ip IP4) Octets() (a, b, c, d byte) {
+	a, b, c, d = byte(ip>>24), byte(ip>>16), byte(ip>>8), byte(ip)
+	return
+}
+
+func (ip IP4) ToIP() net.IP {
+	return net.IPv4(ip.Octets())
+}
+
+func (ip IP4) NetworkOrder() uint32 {
+	if NativelyLittle() {
+		a, b, c, d := byte(ip>>24), byte(ip>>16), byte(ip>>8), byte(ip)
+		return uint32(a) | uint32(b)<<8 | uint32(c)<<16 | uint32(d)<<24
+	} else {
+		return uint32(ip)
+	}
+}
+
+func (ip IP4) String() string {
+	return ip.ToIP().String()
+}
+
+func (ip IP4) StringSep(sep string) string {
+	a, b, c, d := ip.Octets()
+	return fmt.Sprintf("%d%s%d%s%d%s%d", a, sep, b, sep, c, sep, d)
 }
