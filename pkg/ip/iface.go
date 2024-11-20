d7ed1b3816dab1593b0474ddf417700ee18583d9
HyunSu1768 <azxcv1768@gmail.com>
2024-11-20 09:02:53 +0900

    feat(iface.go): IP에 기반한 인터페이스 검색 기능 추가 fix(iface.go): 오류 메시지에서 문법 오류 수정

--- pkg/ip/iface.go
+++ pkg/ip/iface.go
@@ -2,6 +2,7 @@ package ip
 
 import (
 	"errors"
+	"fmt"
 	"github.com/vishvananda/netlink"
 	"net"
 	"syscall"
@@ -57,7 +58,7 @@ func GetInterfaceIP4Addrs(iface *net.Interface) ([]net.IP, error) {
 		return ipAddr, nil
 	}
 
-	return nil, errors.New("Interface에 할당된 IP4 가 없습니다.")
+	return nil, errors.New("Interface에 할당된 IP4 가 없습니다")
 }
 
 func GetInterfaceIP6Addrs(iface *net.Interface) ([]net.IP, error) {
@@ -92,7 +93,7 @@ func GetInterfaceIP6Addrs(iface *net.Interface) ([]net.IP, error) {
 		return ipAddr, nil
 	}
 
-	return nil, errors.New("Interface에 할당된 IP6 가 없습니다.")
+	return nil, errors.New("Interface에 할당된 IP6 가 없습니다")
 }
 
 func GetInterfaceIP4AddrMatch(iface *net.Interface, matchIP net.IP) error {
@@ -109,7 +110,7 @@ func GetInterfaceIP4AddrMatch(iface *net.Interface, matchIP net.IP) error {
 		}
 	}
 
-	return errors.New("Interface에서 주어진 IP4와 일치하는 IP가 존재하지 않습니다.")
+	return errors.New("Interface에서 주어진 IP4와 일치하는 IP가 존재하지 않습니다")
 }
 
 func GetInterfaceIP6AddrMatch(iface *net.Interface, matchIP net.IP) error {
@@ -126,7 +127,7 @@ func GetInterfaceIP6AddrMatch(iface *net.Interface, matchIP net.IP) error {
 		}
 	}
 
-	return errors.New("Interface에서 주어진 IP6와 일치하는 IP가 존재하지 않습니다.")
+	return errors.New("Interface에서 주어진 IP6와 일치하는 IP가 존재하지 않습니다")
 }
 
 func GetDefaultGatewayInterface() (*net.Interface, error) {
@@ -138,13 +139,13 @@ func GetDefaultGatewayInterface() (*net.Interface, error) {
 	for _, route := range routes {
 		if route.Dst == nil || route.Dst.String() == "0.0.0.0/0" {
 			if route.LinkIndex <= 0 {
-				return nil, errors.New("Default Gateway를 찾았지만, Interface를 결정할 수 없습니다.")
+				return nil, errors.New("Default Gateway를 찾았지만, Interface를 결정할 수 없습니다")
 			}
 			return net.InterfaceByIndex(route.LinkIndex)
 		}
 	}
 
-	return nil, errors.New("Defluat Gateway를 찾을 수 없습니다.")
+	return nil, errors.New("Defluat Gateway를 찾을 수 없습니다")
 }
 
 func GetDefaultV6GatewayInterface() (*net.Interface, error) {
@@ -156,11 +157,59 @@ func GetDefaultV6GatewayInterface() (*net.Interface, error) {
 	for _, route := range routes {
 		if route.Dst == nil || route.Dst.String() == "::/0" {
 			if route.LinkIndex <= 0 {
-				return nil, errors.New("Default V6 Gateway를 찾았지만, Interface를 결정할 수 없습니다.")
+				return nil, errors.New("Default V6 Gateway를 찾았지만, Interface를 결정할 수 없습니다")
 			}
 			return net.InterfaceByIndex(route.LinkIndex)
 		}
 	}
 
-	return nil, errors.New("Defluat V6 Gateway를 찾을 수 없습니다.")
+	return nil, errors.New("Defluat V6 Gateway를 찾을 수 없습니다")
+}
+
+func GetInterfaceByIP(ip net.IP) (*net.Interface, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, iface := range ifaces {
+		err := GetInterfaceIP4AddrMatch(&iface, ip)
+		if err == nil {
+			return &iface, nil
+		}
+	}
+	return nil, errors.New("주어진 IP에 맞는 인터페이스가 존재하지 않습니다")
+}
+
+func GetInterfaceByIP6(ip net.IP) (*net.Interface, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, iface := range ifaces {
+		err := GetInterfaceIP6AddrMatch(&iface, ip)
+		if err == nil {
+			return &iface, nil
+		}
+	}
+	return nil, errors.New("주어진 IP6에 맞는 인터페이스가 존재하지 않습니다")
+}
+
+func GetInterfaceBySpecificIPRouting(ip net.IP) (*net.Interface, net.IP, error) {
+	routes, err := netlink.RouteGet(ip)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for _, route := range routes {
+		iface, err := net.InterfaceByIndex(route.LinkIndex)
+		if err != nil {
+			return nil, nil, fmt.Errorf("interface를 찾을 수 없습니다 : %v", err)
+		} else {
+			return iface, route.Src, nil
+		}
+	}
+
+	return nil, nil, errors.New("주어진 IP에 대한 interface를 찾을 수 없습니다")
 }
