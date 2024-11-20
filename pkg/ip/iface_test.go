58f6cc817f5370778627a55672f77330b74e0629
HyunSu1768 <azxcv1768@gmail.com>
2024-11-20 10:36:11 +0900

    test(iface_test.go): EnsureV4AddressOnLink 함수에 대한 단위 테스트 추가 테스트를 통해 IP 주소가 링크에 올바르게 추가되는지 확인하기 위함.

--- pkg/ip/iface_test.go
+++ pkg/ip/iface_test.go
@@ -1,1 +1,54 @@
 package ip
+
+import (
+	"flat/pkg/ns"
+	"github.com/vishvananda/netlink"
+	"net"
+	"syscall"
+	"testing"
+)
+
+func TestEnsureV4AddressOnLink(t *testing.T) {
+	teardown := ns.SetUpNetLinkTest(t)
+	defer teardown()
+
+	lo, err := netlink.LinkByName("lo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if netlink.LinkSetUp(lo); err != nil {
+		t.Fatal(err)
+	}
+
+	ipn := IP4Net{IP: FromIP(net.ParseIP("127.0.0.2")), PrefixLen: 24}
+	if err := EnsureV4AddressOnLink(ipn, ipn, lo); err != nil {
+		t.Fatal(err)
+	}
+
+	addrs, err := netlink.AddrList(lo, syscall.AF_INET)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(addrs) != 1 || addrs[0].String() != "127.0.0.2/24 lo" {
+		t.Fatalf("주소 %v는 예상되지 않습니다", addrs)
+	}
+
+	if err := netlink.AddrAdd(lo, &netlink.Addr{IPNet: &net.IPNet{IP: net.ParseIP("127.0.1.1"), Mask: net.CIDRMask(24, 32)}}); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := EnsureV4AddressOnLink(ipn, ipn, lo); err != nil {
+		t.Fatal(err)
+	}
+
+	addrs, err = netlink.AddrList(lo, syscall.AF_INET)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(addrs) != 2 {
+		t.Fatalf("2개의 주소가 예상되었습니다, addr : %v", addrs)
+	}
+}
